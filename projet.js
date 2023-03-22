54c1a2c806de2847204c30bf38eaa265a746198b
kamenifrancis <blickson57@gmail.com>
2023-03-22 15:34:49 +0100

    checkpoint de DOM

---
+++ projet.js
@@ -0,0 +1,47 @@
+//changement de couleur 
+
+let like=document.getElementsByClassName('fa-regular fa-thumbs-up');
+
+console.log(like,'like')
+
+for(let colors of like){
+  colors.addEventListener('click',function(){
+    if(colors.style.color === 'gray' ){
+      colors.style.color="red"
+    }else{
+      colors.style.color = "gray" 
+    }
+  }) 
+   } 
+   
+//add element 
+//compteur ajout +1 
+
+let plusBtn = document.getElementsByClassName('bttnplus');
+
+console.log(plusBtn);
+
+for (let plus of plusBtn) {
+  plus.addEventListener('click', function () {
+    plus.previousElementSibling.innerText++;
+   
+  });
+
+}
+
+
+//diminuer 
+
+let minusBtn = document.getElementsByClassName('bttnmoin');
+for(let minus of minusBtn){
+
+minus.addEventListener('click', function (){
+
+if ( minus.nextElementSibling.innerText >0 ){
+    minus.nextElementSibling.innerText--;
+}
+
+})
+
+};
+ele
